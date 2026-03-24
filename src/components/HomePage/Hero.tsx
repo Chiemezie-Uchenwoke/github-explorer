4d6859de9c8f99d2140f96a16ca77eeeeed5dada
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-03-24 14:48:25 +0100

    Improve hero animation

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -7,8 +7,8 @@ const HomePageHero = () => {
         <section className="w-19/20 lg:w-9/10 max-w-7xl mx-auto min-h-[60vh] 2xl:min-h-[50vh] flex flex-col gap-6 lg:gap-10 items-center justify-center py-12 relative">
             <motion.h2 
                 className="text-blue-default font-semibold text-3xl md:text-4xl lg:text-6xl text-center w-9/10 max-w-2xl mx-auto "
-                initial={{opacity: 0, y: 10}}
-                animate={{opacity: 1, y: 0, transition: {duration: 0.3}}}
+                initial={{opacity: 0, y: 10, rotate: 5}}
+                animate={{opacity: 1, y: 0, transition: {duration: 0.3}, rotate: 0}}
             > 
                 Discover the Developers Behind the Code 
             </motion.h2>
