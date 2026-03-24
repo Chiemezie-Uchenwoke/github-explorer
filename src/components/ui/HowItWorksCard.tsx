c660de1ac7902ef76c49af821ec1ff550da574fa
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-03-24 15:48:41 +0100

    Adjust work section styling

--- src/components/ui/HowItWorksCard.tsx
+++ src/components/ui/HowItWorksCard.tsx
@@ -10,8 +10,8 @@ const HowItWorksCard = ({icon, title, desc}: HowItWorksProps) => {
     const Icon = icon;
 
     return (
-        <article className="bg-blue-muted py-8 lg:py-10 px-4 lg:px-6 flex flex-col gap-5 rounded-2xl select-none hover:scale-95 transition duration-300 ease-in-out group">
-            <span className="w-12 h-12 flex justify-center items-center bg-primary rounded-lg text-text-primary/80 text-xl border border-text-primary/25 group-hover:animate-pulse">
+        <article className="bg-blue-muted py-8 lg:py-10 px-4 lg:px-6 flex flex-col gap-5 rounded-2xl select-none hover:scale-95 transition duration-300 ease-in-out border border-blue-light/10 shadow-lg shadow-text-primary/3">
+            <span className="w-12 h-12 flex justify-center items-center bg-primary rounded-lg text-text-primary/80 text-xl border border-text-primary/25">
                 <Icon />
             </span>
 
