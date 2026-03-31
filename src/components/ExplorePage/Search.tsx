2caada6e272e125805d5061880a996682387de77
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-03-31 10:11:41 +0100

    Style explore page and make footer sit nicely

--- src/components/ExplorePage/Search.tsx
+++ src/components/ExplorePage/Search.tsx
@@ -5,15 +5,19 @@ interface SearchGitHubUserProps {
 
 const SearchGitHubUser = ({searchText, setSearchText}: SearchGitHubUserProps) => {
     return (
-        <section>
+        <section className="my-12 py-8 w-full max-w-lg mx-auto flex flex-col gap-8 items-center">
             <input 
                 type="search"
                 placeholder="Enter a github username..." 
                 value={searchText}
                 onChange={(e) => setSearchText(e.target.value)} 
+                className="w-9/10 border border-text-primary placeholder:text-text-primary/70 px-3 h-12 lg:h-16 rounded text-text-primary/80"
             />
 
-            <button type="submit">
+            <button 
+                type="submit"
+                className="bg-blue-default text-text-primary py-3 px-8 rounded-2xl w-9/10 font-medium active:translate-y-1 cursor-pointer hover:translate-y-0.5 transition duration-200"
+            >
                 Search
             </button>
         </section>
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -8,15 +8,15 @@ interface MainLayoutProps {
 
 const MainLayout = ({children}: MainLayoutProps) => {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <NavBar />
 
-            <main>
+            <main className="flex-1">
                 {children}
             </main>
 
             <Footer />
-        </>
+        </div>
     )
 }
 
