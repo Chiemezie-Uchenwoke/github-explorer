e236e719945b9d318be916144ecf6d9f61cfd875
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-04-10 20:58:53 +0100

    Complete project

--- src/components/HomePage/Features.tsx
+++ src/components/HomePage/Features.tsx
@@ -3,7 +3,7 @@ import FeaturesCard from "../ui/FeaturesCard";
 
 const Features = () => {
     return (
-        <section className="bg-secondary">
+        <section className="bg-secondary w-full">
             <div className="w-19/20 lg:w-9/10 max-w-7xl mx-auto flex flex-col gap-10 lg:gap-14 items-center py-16 lg:py-20">
                 <h2 className="text-text-primary/90 font-semibold text-2xl lg:text-3xl capitalize text-center">Features you can explore</h2>
 
